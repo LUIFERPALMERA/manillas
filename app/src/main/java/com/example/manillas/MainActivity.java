5a525673b70e6f8f61b23696d5725f20a75f5445
unknown <lpalmera@unimagdalena.edu.co>
2019-07-11 21:42:39 -0500

    Spinners con la información

--- app/src/main/java/com/example/manillas/MainActivity.java
+++ app/src/main/java/com/example/manillas/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.manillas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String materiales[];
+        String dijes[];
+        String tipos[];
+        String monedas[];
+
+        Spinner comboMateriales, comboDijes, comboTipos, comboMonedas;
+
+        // Traemos información de los arrays
+        materiales = getResources().getStringArray(R.array.materiales);
+        dijes = getResources().getStringArray(R.array.dijes);
+        tipos = getResources().getStringArray(R.array.tipos);
+        monedas = getResources().getStringArray(R.array.monedas);
+
+        //Creamos el adapter indicando donde se va a colocar
+        // Como se va a visualizar y que se va a mostrar
+        ArrayAdapter<String> adapterMateriales = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,materiales);
+        ArrayAdapter<String> adapterDijes = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,dijes);
+        ArrayAdapter<String> adapterTipos = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,tipos);
+        ArrayAdapter<String> adapterMonedas = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,monedas);
+
+        //Pasamos el adapter al combo
+        comboMateriales = findViewById(R.id.spinnerMateriales);
+        comboMateriales.setAdapter(adapterMateriales);
+
+        comboDijes = findViewById(R.id.spinnerDijes);
+        comboDijes.setAdapter(adapterDijes);
+
+        comboTipos = findViewById(R.id.spinnerTipos);
+        comboTipos.setAdapter(adapterTipos);
+
+        comboMonedas = findViewById(R.id.spinnerMonedas);
+        comboMonedas.setAdapter(adapterMonedas);
     }
 }
